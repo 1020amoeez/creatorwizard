12abbe8e32a7183ff440492ce037d8903669be8f
hamzach625 <hamzach625@gmail.com>
2024-03-08 21:00:22 +0500

    ui issues fixed

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -16,25 +16,27 @@ const Footer = () => {
               <div className="footerlinks">
                 <div className="footerlinksinner">
                   <h6 className="footerhead">Company</h6>
-                  <Link href="https://wizard-nft.vercel.app/aboutus" className="footerpara">About Us</Link>
+                  <a href="https://wizard-user-mainnet.vercel.app/aboutus" target='_blank' className="footerpara">About Us</a>
                 </div>
                 <div className="footerlinksinner">
                 <h6 className="footerhead">Marketplace</h6>
-                <Link href="https://wizard-nft.vercel.app/discovercollection" className="footerpara">Explore Collections</Link>
-                <Link href="https://wizard-nft.vercel.app/launchpad" className="footerpara">Launchpad</Link>
-                <Link href="/createnewcollections" className="footerpara">Create Collection</Link>
+                <a target='_blank' href="https://wizard-user-mainnet.vercel.app/discovercollection" className="footerpara">Explore Collections</a>
+                <a target='_blank' href="https://wizard-user-mainnet.vercel.app/launchpad" className="footerpara">Launchpad</a>
+                <Link href="/mycollection" className="footerpara">Create Collection</Link>
               </div>
                 <div className="footerlinksinner">
                   <h6 className="footerhead">Resources</h6>
-                  <Link href="/termsandcondition" className="footerpara">Terms of Service</Link>
-                  <Link href="/privacypolicy" className="footerpara">Privacy Policy</Link>
+                  <a href="https://wizard-user-mainnet.vercel.app/faqs" target='_blank' className="footerpara">FAQS</a>
+                  <a href="https://wizard-user-mainnet.vercel.app/walletguide" target='_blank' className="footerpara">Wallet Guide</a>
+                  <a href="https://wizard-user-mainnet.vercel.app/termsandcondition" target='_blank' className="footerpara">Terms of Service</a>
+                  <a href="https://wizard-user-mainnet.vercel.app/privacypolicy" target='_blank' className="footerpara">Privacy Policy</a>
                 </div>
                 <div className="footerlinksinner">
                   <h6 className="footerhead">follow</h6>
-                  <p className="footerpara">Twitter</p>
-                  <p className="footerpara">Discord</p>
-                  <p className="footerpara">Youtube</p>
-                  <p className="footerpara">Instagram</p>
+                  <a href='https://twitter.com/wizardgallery_' target='_blank' className="footerpara">Twitter</a>
+                  <a href='https://t.me/+i7xcJ-EBosFjYjVk' target='_blank' className="footerpara">TG Announcement</a>
+                  <a href='https://t.me/+76DP_bkMEUpjMzNk' target='_blank' className="footerpara">Telegram</a>
+                  <a href='https://medium.com/@wizardgallery.xyz' target='_blank' className="footerpara">Medium</a>
                 </div>
               </div>
             </div>
