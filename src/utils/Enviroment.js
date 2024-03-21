fc9b1f96519ed72b3d707b9df33a620a35f86e43
1020amoeez <113603154+1020amoeez@users.noreply.github.com>
2024-03-21 19:51:06 +0500

    new

--- src/utils/Enviroment.js
+++ src/utils/Enviroment.js
@@ -1,6 +1,6 @@
 let Environment = {
-  api_url: "https://wizardapi.quecko.com",
-  // api_url: "http://192.168.20.69:8080",
+  // api_url: "https://wizardapi.quecko.com",
+  api_url: "http://192.168.20.69:8080",
 };
 
 export default Environment;
