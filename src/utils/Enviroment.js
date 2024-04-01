2379ef779c41d96f3aaedb0bb693048c06cefa41
1020amoeez <113603154+1020amoeez@users.noreply.github.com>
2024-04-02 00:37:30 +0500

    merger with main1

--- src/utils/Enviroment.js
+++ src/utils/Enviroment.js
@@ -1,6 +1,6 @@
 let Environment = {
-  // api_url: "https://wizardapi.quecko.com",
-  api_url: "http://192.168.20.49:8080",
+  api_url: "https://wizardapi.quecko.com",
+  // api_url: "http://192.168.20.49:8080",
   // api_url: "https://api.wizardgallery.xyz",
 };
 
