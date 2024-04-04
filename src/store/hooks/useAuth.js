3c50b1a01b874f2524a2d05d86586f2e0f3cf250
1020amoeez <113603154+1020amoeez@users.noreply.github.com>
2024-04-04 23:12:14 +0500

    latest mobile

--- src/store/hooks/useAuth.js
+++ src/store/hooks/useAuth.js
@@ -1,45 +1,42 @@
 import { connectorsByName } from "../utils/web3React";
 import { setupNetwork } from "../utils/wallet";
 
-
 const useAuth = () => {
   const login = async (connectorID, chain) => {
     const connector = connectorsByName[connectorID];
     if (connector) {
-      if (chain === '1') {
-        console.log(chain, 'Ethereum Mainnet');
+      if (chain === "1116") {
+        // console.log(chain, 'Ethereum Mainnet');
         const hasSetup = await setupNetwork(chain);
         if (hasSetup) {
           await connector.activate(parseInt(chain));
         }
-      } else if (chain === '56') {
-        console.log(chain, 'Binance Smart Chain Mainnet');
+      } else if (chain === "56") {
+        console.log(chain, "Binance Smart Chain Mainnet");
         const hasSetup = await setupNetwork(chain);
         if (hasSetup) {
           await connector.activate(parseInt(chain));
         }
       }
-    } else if (chain === '5') {
-      console.log(chain, 'Goerli');
+    } else if (chain === "5") {
+      console.log(chain, "Goerli");
       const hasSetup = await setupNetwork(chain);
       if (hasSetup) {
         await connector.activate(parseInt(chain));
       }
     } else {
-      console.error('Unsupported chain');
+      console.error("Unsupported chain");
     }
-
   };
 
-
   const logout = async () => {
-    const connectorID = window.localStorage.getItem("connectorId")
+    const connectorID = window.localStorage.getItem("connectorId");
     const connector = connectorsByName[connectorID];
     if (connector) {
       if (connector?.deactivate) {
-        await connector.deactivate()
+        await connector.deactivate();
       } else {
-        await connector.resetState()
+        await connector.resetState();
       }
     } else {
       // toastError("Can't find connector", "The connector config is wrong");
